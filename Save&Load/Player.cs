b73aed39a6c3a66510ff6d052cdab0ec2d8b7d50
Aditya Sharma <55913952+Aditya-021@users.noreply.github.com>
2021-01-30 21:26:01 +0530

    Add files via upload

---
+++ Save&Load/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+    public int points;
+
+   /* private void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            SavePlayer();
+        }
+        if(Input.GetKey(KeyCode.L))
+        {
+            LoadPlayer();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            health += 1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            points += 1;
+        }
+    }*/
+
+    public void SavePlayer()
+    {
+        SaveData.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveData.LoadPlayer();
+        health = data.health;
+        points = data.points;
+    }
+}
---
+++ Save&Load/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]// ****Now we can able to save it as a file.
+public class PlayerData 
+{
+    public int health;
+    public int points;
+
+    public PlayerData(Player player)
+    {
+        health = player.health;
+        points = player.points;
+    }
+}
